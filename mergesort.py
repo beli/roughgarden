1f4e8a3c263597bb54e6b15205f2e73ce0bcf163
beli <imnutsdoc@gmail.com>
2021-10-15 19:12:21 +0530

    Added assertion

--- mergesort.py
+++ mergesort.py
@@ -1,14 +1,11 @@
 def sort(l):
     mid = len(l)//2
-    if mid == 0:
-        return l
-    return merge(sort(l[:mid]), sort(l[mid:]))
+    return l if mid == 0 else merge(sort(l[:mid]), sort(l[mid:]))
 
 def merge(l1, l2):
     l = []
-    while l1 and l2:
-        l.append( l1.pop(0) if l1[0] < l2[0] else l2.pop(0) )
+    while l1 and l2: l.append( l1.pop(0) if l1[0] < l2[0] else l2.pop(0) )
     l += l1 if l1 else l2
     return l 
 
-print(sort(list(range(20)[::-1])))
+assert(sort([20, 20, 20] + list(range(20)[::-1])) == list(range(20)) + [20, 20, 20])
