71cb5dc13ce7e604b577f91949ae737e2fe2dff4
beli <imnutsdoc@gmail.com>
2021-10-16 19:34:02 +0530

    Typo

--- inversions.py
+++ inversions.py
@@ -8,7 +8,7 @@ def count(l):
 def merge(l1, l2):
     splits, l = 0, []
     while l1 and l2: 
-        n, n_split = (l1.pop(0), 0) if l1[0] < l2[0] else (l2.pop(0), len(l1)) 
+        n, n_split = (l1.pop(0), 0) if l1[0] <= l2[0] else (l2.pop(0), len(l1)) 
         l.append(n)
         splits += n_split
     l += l1 if l1 else l2
