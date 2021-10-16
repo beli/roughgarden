9c7534115139c1f9f914a8610599ab098ca2b4f3
beli <imnutsdoc@gmail.com>
2021-10-16 20:01:40 +0530

    Deleted line

--- inversions.py
+++ inversions.py
@@ -10,7 +10,6 @@ def merge(l1, l2):
         n, n_split = (l1.pop(0), 0) if l1[0] <= l2[0] else (l2.pop(0), len(l1)) 
         l.append(n)
         splits += n_split
-    l += l1 if l1 else l2
-    return splits, l 
+    return splits, l + (l1 if l1 else l2)
 
 assert(count([1, 3, 5, 2, 4, 6]) == (3, [1, 2, 3, 4, 5, 6]))
--- mergesort.py
+++ mergesort.py
@@ -4,7 +4,6 @@ def sort(l):
 def merge(l1, l2):
     l = []
     while l1 and l2: l.append( l1.pop(0) if l1[0] <= l2[0] else l2.pop(0) )
-    l += l1 if l1 else l2
-    return l 
+    return l + (l1 if l1 else l2)
 
 assert( sort(list(range(20)[::-1])) == list(range(20)) )
