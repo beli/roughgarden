c8345535d02674b8cbaf537afccfe80f8e143a6c
beli <imnutsdoc@gmail.com>
2021-10-16 15:21:37 +0530

    Removed extra merge

--- inversions.py
+++ inversions.py
@@ -14,19 +14,4 @@ def merge(l1, l2):
     l += l1 if l1 else l2
     return splits, l 
 
-def mergek(l1, l2):
-    splits, i, j, l = 0, 0, 0, []
-    ln1, ln2 = len(l1), len(l2)
-    while i < ln1 and j < ln2:
-        if l1[i] <= l2[j]:
-            n = l1[i]
-            i += 1
-        else: 
-            n = l2[j]
-            j += 1
-            splits += ln1 - i
-        l.append(n)
-    l += l1[i:] if j == ln2 else l2[j:]
-    return splits, l
-
 assert(count([1, 3, 5, 2, 4, 6]) == (3, [1, 2, 3, 4, 5, 6]))
