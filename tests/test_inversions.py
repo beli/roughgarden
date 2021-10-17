e20ca42bd77939caee7e483ad48060bedd7d6509
beli <imnutsdoc@gmail.com>
2021-10-17 22:44:20 +0530

    Black

--- tests/test_inversions.py
+++ tests/test_inversions.py
@@ -1,7 +1,9 @@
 import inversions
 
+
 def test_merge():
-    assert inversions.merge([1,2,3,4,5], [1,6,7]) == (4, [1,1,2,3,4,5,6,7])
+    assert inversions.merge([1, 2, 3, 4, 5], [1, 6, 7]) == (4, [1, 1, 2, 3, 4, 5, 6, 7])
+
 
 def test_count():
-    assert inversions.count([1,2,3,4,5,1,6,7]) == (4, [1,1,2,3,4,5,6,7])
+    assert inversions.count([1, 2, 3, 4, 5, 1, 6, 7]) == (4, [1, 1, 2, 3, 4, 5, 6, 7])
--- tests/test_mergesort.py
+++ tests/test_mergesort.py
@@ -1,7 +1,9 @@
 import mergesort
 
+
 def test_merge():
-    assert mergesort.merge([1, 2], [3, 4, 5]) == [1 ,2, 3, 4, 5]
+    assert mergesort.merge([1, 2], [3, 4, 5]) == [1, 2, 3, 4, 5]
+
 
 def test_sort():
     assert mergesort.sort([3, 2, 1]) == [1, 2, 3]
