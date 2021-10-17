34e3c0bb3ff64b99e53119905d5023e5ad921fc6
beli <imnutsdoc@gmail.com>
2021-10-17 22:24:43 +0530

    Black

--- inversions.py
+++ inversions.py
@@ -1,15 +1,18 @@
 from typing import Tuple
 
+
 def count(l: list) -> Tuple[int, list]:
-    if (mid := len(l)//2) == 0: return 0, l
+    if (mid := len(l) // 2) == 0:
+        return 0, l
     (left, l1), (right, l2) = count(l[:mid]), count(l[mid:])
     cross, l = merge(l1, l2)
     return left + right + cross, l
 
+
 def merge(l1: list, l2: list) -> Tuple[int, list]:
     cross, l = 0, []
-    while l1 and l2: 
-        n, cross_n = (l1.pop(0), 0) if l1[0] <= l2[0] else (l2.pop(0), len(l1)) 
+    while l1 and l2:
+        n, cross_n = (l1.pop(0), 0) if l1[0] <= l2[0] else (l2.pop(0), len(l1))
         l.append(n)
         cross += cross_n
     return cross, l + (l1 if l1 else l2)
--- mergesort.py
+++ mergesort.py
@@ -1,7 +1,9 @@
 def sort(l: list) -> list:
-    return l if (mid := len(l)//2) == 0 else merge( sort(l[:mid]), sort(l[mid:]) )
+    return l if (mid := len(l) // 2) == 0 else merge(sort(l[:mid]), sort(l[mid:]))
+
 
 def merge(l1: list, l2: list) -> list:
     l = []
-    while l1 and l2: l.append( l1.pop(0) if l1[0] <= l2[0] else l2.pop(0) )
+    while l1 and l2:
+        l.append(l1.pop(0) if l1[0] <= l2[0] else l2.pop(0))
     return l + (l1 if l1 else l2)
