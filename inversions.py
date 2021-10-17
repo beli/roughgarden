88efb4570ffaee85f4e212f4648359548bc9fed5
beli <imnutsdoc@gmail.com>
2021-10-17 19:02:50 +0530

    Spelling

--- inversions.py
+++ inversions.py
@@ -1,15 +1,15 @@
 def count(l):
     if (mid := len(l)//2) == 0: return 0, l
     (left, l1), (right, l2) = count(l[:mid]), count(l[mid:])
-    splits, l = merge(l1, l2)
-    return left + right + splits, l
+    cross, l = merge(l1, l2)
+    return left + right + cross, l  # left, right, and cross inversions
 
 def merge(l1, l2):
-    splits, l = 0, []
+    cross, l = 0, []
     while l1 and l2: 
-        n, n_splits = (l1.pop(0), 0) if l1[0] <= l2[0] else (l2.pop(0), len(l1)) 
+        n, cross_n = (l1.pop(0), 0) if l1[0] <= l2[0] else (l2.pop(0), len(l1)) 
         l.append(n)
-        splits += n_splits
-    return splits, l + (l1 if l1 else l2)
+        cross += cross_n
+    return cross, l + (l1 if l1 else l2)
 
 assert(count([1, 3, 5, 2, 4, 6]) == (3, [1, 2, 3, 4, 5, 6]))
