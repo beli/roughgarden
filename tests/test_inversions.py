ca969027d00d803d46ef75964112b2516e1096a6
beli <imnutsdoc@gmail.com>
2021-10-18 10:51:05 +0530

    More hypothesis

--- tests/test_inversions.py
+++ tests/test_inversions.py
@@ -1,9 +1,7 @@
-import inversions
+import inversions, naive_inversions
+from hypothesis import given, strategies as st
 
 
-def test_merge():
-    assert inversions.merge([1, 2, 3, 4, 5], [1, 6, 7]) == (4, [1, 1, 2, 3, 4, 5, 6, 7])
-
-
-def test_count():
-    assert inversions.count([1, 2, 3, 4, 5, 1, 6, 7]) == (4, [1, 1, 2, 3, 4, 5, 6, 7])
+@given(st.lists(st.integers()))
+def test_count(l):
+    assert inversions.count(l)[0] == naive_inversions.count(l)
