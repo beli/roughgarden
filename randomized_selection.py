c845d278bad29c92ce8f9b6e38aa2425d4c59e81
beli <imnutsdoc@gmail.com>
2022-07-08 12:30:34 +0530

    renamed variables

--- randomized_selection.py
+++ randomized_selection.py
@@ -1,12 +1,12 @@
-def select(l: list, o: int):
+def select(sequence: list, order: int):
     # l is the current list, o is current order statistic
-    pivot = l[0]
-    left = [e for e in l[1:] if e <= pivot]
-    right = [e for e in l[1:] if e > pivot]
+    pivot = sequence[0]
+    left = [e for e in sequence[1:] if e <= pivot]
+    right = [e for e in sequence[1:] if e > pivot]
     pivot_index = len(left)
-    if pivot_index > o - 1:
-        return select(left, o)
-    elif pivot_index < o - 1:
-        return select(right, (o - 1) - pivot_index)
+    if pivot_index > order - 1:
+        return select(left, order)
+    elif pivot_index < order - 1:
+        return select(right, (order - 1) - pivot_index)
     else:
         return pivot
